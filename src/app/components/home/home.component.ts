fb4b2481995293a864c77edadeeb5e16b192864d
slscott9 <baseballdude9cf@gmail.com>
2022-08-05 08:46:30 -0500

    Adding flip animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,32 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Card, cards } from 'src/app/interfaces/types';
+declare var $: any
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   cards: any[] = cards
 
-  deckCount: number = 1;
+  deckCount: number = 0;
 
+  cardsDealt: boolean = false;
   dealerCards: Card[] = [];
   playerCards: Card[] = [];
 
   showPlayerCardCount: boolean = false;
   showDealerCardCount: boolean = false;
 
+  @ViewChild('front')
+  front!: ElementRef;
+
+  @ViewChild('back')
+  back!: ElementRef;
+
+
   constructor() { }
 
   ngOnInit(): void {
     this.shuffle()
-    this.addPlayerCard();
-    this.incremnetDeckCount();
-    this.addDealerCard();
-    this.incremnetDeckCount();
-    this.addDealerCard();
+    this.dealCards()
+
+    // this.addPlayerCard();
+    // this.incremnetDeckCount();
+    // this.addDealerCard();
+    // this.incremnetDeckCount();
+    // this.addDealerCard();
+  }
+
+  ngAfterViewInit(): void {
+    // this.handleFlip()
   }
 
   reset() {
@@ -48,7 +63,7 @@ export class HomeComponent implements OnInit {
   }
 
   incremnetDeckCount() {
-    if(this.deckCount === 52) {
+    if (this.deckCount === 52) {
       this.deckCount = 1;
       this.shuffle();
     } else {
@@ -57,13 +72,13 @@ export class HomeComponent implements OnInit {
   }
 
   addPlayerCard() {
-    this.playerCards.push(this.cards[this.deckCount]);
     this.incremnetDeckCount(); //needs work since last card will shuffle automatically
+    this.playerCards.push(this.cards[this.deckCount]);
   }
 
   addDealerCard() {
-    this.dealerCards.push(this.cards[this.deckCount]);
     this.incremnetDeckCount(); //needs work since last card will shuffle automatically
+    this.dealerCards.push(this.cards[this.deckCount]);
   }
 
   togglePlayerCardCount() {
@@ -77,17 +92,37 @@ export class HomeComponent implements OnInit {
   calculateCardCount(card: Card[]) {
     let cardCount = 0;
 
-    for(let i = 0; i < card.length; i++) {
+    for (let i = 0; i < card.length; i++) {
       cardCount += card[i].cardValue
     }
 
     return cardCount;
   }
 
+  dealCards() {
+    this.playerCards = []
+    this.dealerCards = []
+    this.deckCount += 1
+    this.playerCards.push(this.cards[this.deckCount])
+    this.deckCount += 1
+    this.dealerCards.push(this.cards[this.deckCount])
+    this.deckCount += 1
+    this.playerCards.push(this.cards[this.deckCount])
+    // this.deckCount += 1
+    // this.dealerCards.push(this.cards[this.deckCount])
+    // this.cardsDealt = true;
+    // this.handleFlip()
+  }
+
 
 
   calculateWinner() {
-    
+
+  }
+
+  handleFlip() {
+    this.front?.nativeElement.classList.toggle('flipped')
+    this.back?.nativeElement.classList.toggle('flipped')
   }
 
 
